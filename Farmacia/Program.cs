4dd2a52979068e9a9587ea4817c47d14b464ebef
Isamar19 <xiomaraloaiza18@gmail.com>
2022-05-31 20:21:02 -0500

    se permite petición al api desde js

--- Farmacia/Program.cs
+++ Farmacia/Program.cs
@@ -1,3 +1,6 @@
+using Farmacia.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,16 +9,30 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<LaboratorioContext>(options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("FarmaciaContext")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTodo", builder => {
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.         
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+
+app.UseCors("PermitirTodo");
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
